fbcaf6af98f8a4c83c5654b5db3a92d255524672
schinken <schinken@bamberg.ccc.de>
2020-12-28 12:55:33 +0100

    Initial commit

---
+++ config/default.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    ftp: {
+        url: 'ftp://127.0.0.1:9021',
+        credentials: {
+            'username': 'password'
+        }
+    },
+
+    telegram: {
+        token: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+        chatIds: [-1337]
+    }
+};
---
+++ index.js
@@ -0,0 +1,43 @@
+const config = require('./config/production');
+const TelegramProxy = require('./lib/TelegramProxy');
+const MemoryFileSystem = require('./lib/MemoryFileSystem');
+
+const FtpSrv = require('ftp-srv');
+
+const telegramProxy = new TelegramProxy(config.telegram);
+
+const anonymous = (Object.keys(config.ftp.credentials).length === 0);
+const ftpServer = new FtpSrv({
+    url: config.ftp.url,
+    pasv_url: config.ftp.url,
+    anonymous: anonymous,
+});
+
+ftpServer.on('login', ({connection, username, password}, resolve, reject) => {
+
+    if (!anonymous) {
+        if (!config.ftp.credentials[username] || config.ftp.credentials[username] !== password) {
+            return reject();
+        }
+    }
+
+    const memoryFileSystem = new MemoryFileSystem();
+
+    connection.on('STOR', (error, filename) => {
+        const stream = memoryFileSystem.getUploaded(filename);
+
+        if (stream) {
+            telegramProxy.send(filename, stream);
+            memoryFileSystem.removeUploaded(filename);
+        }
+    });
+
+    return resolve({
+        fs: memoryFileSystem
+    });
+});
+
+
+ftpServer
+    .listen()
+    .then(_ => console.log("Server is running"));
