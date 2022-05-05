40a0c57beda4d048fc9cf959605e2077586d902f
schinken <mail+github@schinken.io>
2022-05-05 11:22:34 +0200

    Try to determine pasv-mode ip ourself

--- config/default.js
+++ config/default.js
@@ -1,11 +1,14 @@
 module.exports = {
     ftp: {
         url: 'ftp://0.0.0.0:21',
+
+        /* Optional! Only needed if you want to specify the pasv ip yourself
         pasv: {
             ip: '192.168.1.2',
             portMin: 21000,
             portMax: 21010,
         },
+        */
         credentials: {
             'username': 'password'
         }
